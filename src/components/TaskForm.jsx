ae23df989405ad4c6d86e932962c9a5e8147a56e
Andy Louis <lowkeyalways@MacBook-Pro-de-Lowkey.local>
2025-10-22 15:03:11 +0200

    TaskForm done

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function TaskForm ( onAddTask ) {
+    const [text, setText] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const value = text.trim();
+        if (!value) return;       
+        onAddTask(value);         
+        setText("");
+      };
+
+    return(
+        <form onSubmit={ handleSubmit }>
+            <input
+                type="text"
+                placeholder="Nouvelle tâche..."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+      <button type="submit">Ajouter</button>
+        </form>
+    )
+
+}
+
+export default TaskForm;
