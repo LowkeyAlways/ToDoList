b4b90043327d222d9bba812d67a8e6808d55cb3c
Andy Louis <lowkeyalways@MacBook-Pro-de-Lowkey.local>
2025-10-22 15:35:39 +0200

    App done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from 'react'
 import './App.css'
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
 
+  const addTask = (text) => {
+    const newTask = {
+      id: Date.now(),
+      text,
+      done: false
+    }
+    setTasks((prev) => [...prev, newTask])
+  }
+
+  const toggleTask = (id) => {
+    setTasks((prev) => 
+    prev.map((t) => (
+      t.id === id ? {...t, done: !t.done} : t
+    )))
+  }
+
+  const deleteTask = (id) => {
+    setTasks((prev) => prev.filter((t) => t.id !== id))
+  }
   return (
-    <>
+    <div>
       <h1>Ma journée productive</h1>
-    </>
+      <TaskForm onAddTask={addTask} />
+      <TaskList tasks={tasks} onToggleTask={toggleTask} onDeleteTask={deleteTask}/>
+    </div>
   )
 }
 
